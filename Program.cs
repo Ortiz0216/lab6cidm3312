f974ee1747e8ea3d0bbdff945bdacd436c9eb71b
Daniel Ortiz <do973658@wtstudents.wtamu.edu>
2019-02-14 13:24:53 -0600

    second commit

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ namespace lab6
                     {
                         new Movie { Title = "Avatar", Genre = "Action" },
                         new Movie { Title = "Deadpool", Genre = "Action" },
-                        new Movie { Title = "Apollo 13 ", Genre = "Drama" },
+                        new Movie { Title = "Apollo 13", Genre = "Drama" },
                         new Movie { Title = "The Martian", Genre = "Sci-Fi" }
                     }
                 };
@@ -46,28 +46,24 @@ namespace lab6
             // Add posts to an existing blog by updating post.Blog object
             using (var db = new Context())
             {
-                Movie movie = new Movie { Title = "Intro to JS. NEW!"};
-                //Blog blogToUpdate = db.Blogs.Include(b => b.Posts).First();
-                //blogToUpdate.Posts.Add(post);
-                movie.Studio = db.Studios.First();
+                Movie movie = new Movie { Title = "Jurassic Park", Genre = "Action"};
+                movie.Studio = db.Studios.Where(b => b.Name == "Universal Pictures").First();
                 db.Add(movie);
                 db.SaveChanges();
             }
 
-            // Add post to an existing blog by adding it to the List<Post>
+            // Move post from one studio to another
             using (var db = new Context())
             {
-                Movie post = new Movie { Title = "My First Coding Book for Babies"};
-                Studio blogToUpdate = db.Studios.Include(b => b.Movies).Where(b => b.Name == "Blog 2").First();
-                blogToUpdate.Movies.Add(post);
+                Movie Movie = db.Movies.Where(p => p.Title == "Apollo 13").First();
+                Movie.Studio = db.Studios.Where(b => b.Name == "Universal Pictures").First();
+                //db.Add(Movie);
                 db.SaveChanges();
             }
-
-            // Move post from one blog to another
             using (var db = new Context())
             {
-                Movie movie = db.Movies.Where(p => p.Title == "Intro to JS. NEW!").First();
-                movie.Studio = db.Studios.Where(b => b.Name == "Blog 2").First();
+                Movie Movie = db.Movies.Where(p => p.Title == "Deadpool").First();
+                db.Remove(Movie);
                 db.SaveChanges();
             }
             
@@ -83,37 +79,6 @@ namespace lab6
                        Console.WriteLine("\t" + p);
                     }
                 }
-            }
-
-            // Advanced Challenge: Filter on child entity with projection
-            // GroupJoin() and SelectMany() might also be useful
-            // Or Explicit loading
-            //  db.Entry(b).Collection(x => x.Posts).Load(); // explicit loads
-            using (var db = new Context())
-            {
-                // I want to select all blogs that have a post containing C# in the title
-                // and just those posts
-                // var query = db.Posts.Where(p => p.Title.Contains("C#")) // This will give you the posts, but not blogs
-                
-                // This gives blogs with only those C# posts but as a new anonymous type
-                var blogsWithCSharpPosts = db.Studios.Include(b => b.Movies).Select(b => new
-                    {
-                        Name = b.Name,
-                        // Filter on child entity within the select projection
-                        Movies = b.Movies.Where(p => p.Title.Contains("C#")).ToList()
-                    }).Where(b => b.Movies.Count() > 0);
-                
-                Console.WriteLine("\nPosts just containing C# with their blog: ");
-                foreach (var b in blogsWithCSharpPosts)
-                {
-                    Console.WriteLine($"Blog Url: {b.Name}");
-                    foreach (var p in b.Movies)
-                    {
-                        Console.WriteLine($"\t{p.Title}");
-                    }
-                }
-
-
             }
         }
     }
